0229546a60d219d6183eacd50706458f139868a8
hjkimGithub <rlaguswo3408@gmail.com>
2022-01-02 18:43:37 +0900

    genres.map -> genres && genres.map

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -13,7 +13,7 @@ function Movie({ id, coverImg, title, year, summary, genres }) {
         <h3 className={styles.movie__year}>{year}</h3>
         <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
         <ul className={styles.movie__genres}>
-          {genres.map((g) => (
+          {genres && genres.map((g) => (
             <li key={g}>{g}</li>
           ))}
         </ul>
